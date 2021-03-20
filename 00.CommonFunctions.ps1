6849fd621524852cae0a992139ed3374b1a0b395
Rahul Sarkar <rsarkar@sxiq.com.au>
2021-03-21 08:31:32 +1100

    Refactoring and adding Meslo Nerd Fonts install

--- 00.CommonFunctions.ps1
+++ 00.CommonFunctions.ps1
@@ -31,7 +31,6 @@ function DownloadFromGithubReleasePage($releasePageUrl, $setupFilePatternName) {
     Write-Host ++++++++++ LatestReleaseFileName = $LatestReleaseFileName
     $LatestReleaseDownloadUrl = "https://github.com$($LatestReleaseUrl.href)"
     Write-Host ++++++++++ LatestReleaseDownloadUrl = $LatestReleaseDownloadUrl
-    cd ~/Downloads
     if (Test-Path -Path $LatestReleaseFileName) {
         Remove-Item $LatestReleaseFileName
         Write-Host ++++++++++ Removing existing file with same name - $LatestReleaseFileName
@@ -39,4 +38,40 @@ function DownloadFromGithubReleasePage($releasePageUrl, $setupFilePatternName) {
     Write-Host ++++++++++ Downloading $LatestReleaseFileName
     Invoke-WebRequest -Uri $LatestReleaseDownloadUrl -OutFile $LatestReleaseFileName
     return $LatestReleaseFileName
+}
+
+Function Invoke-InstallFonts($FolderPath, $FontsPatternToInstall) {
+    $Destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
+    $SourceDir = ".\$FolderPath\"
+
+    $TempFolder = "~/Downloads/TempFonts"
+    $FontInstallPath = "C:\Windows\Fonts"
+
+    if (Test-Path -Path $TempFolder ) {
+        Remove-Item $TempFolder -Recurse -Force
+    }
+    New-Item $TempFolder -Type Directory -Force | Out-Null
+    $FONTS = 0x14
+    $objShell = New-Object -ComObject Shell.Application
+    $objFolder = $objShell.Namespace($FONTS)
+    Get-ChildItem -Path $SourceDir -Include $FontsPatternToInstall -Recurse | ForEach {
+        $File = $_
+        $try = $true
+        $installedFonts = @(Get-ChildItem $FontInstallPath | Where-Object { $_.PSIsContainer -eq $false } | Select-Object basename)
+        $name = $File.baseName
+
+        foreach ($font in $installedFonts) {
+            $font = $font -replace "_", ""
+            $name = $name -replace "_", ""
+            if ($font -match $name) {
+                $try = $false
+            }
+        }
+    
+        if ($try) {
+            $objFolder.CopyHere($File.fullname)
+            Write-Host ++++++++++ Installed $File.fullname
+        }
+    
+    }
 }
--- 04.MisclAppInstall.ps1
+++ 04.MisclAppInstall.ps1
@@ -1,9 +1,51 @@
-﻿# Install Powershell Preview Latest version
+﻿#Navigate to Downloads folder to download and do stuff
+cd ~/Downloads
+# Install Powershell Preview Latest version
 $pwshPreviewFilename = DownloadFromGithubReleasePage https://github.com/PowerShell/powershell/releases *powershell*preview*win*x64*
 InstallMSI $pwshPreviewFilename
 # Install posh-git and oh-my-posh
 Install-Module posh-git -Force
 Install-Module oh-my-posh -Force
+Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
+
+# Install Powershell Preview's history lookup and Az.Predictor 
+Install-Module -Name PSReadLine -AllowPrerelease -Force -SkipPublisherCheck
+Install-module -name Az.Tools.Predictor -RequiredVersion 0.2.0 -Force
+Enable-AzPredictor -AllSession
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Install Cascadia Code PL Fonts
+$FileName = DownloadFromGithubReleasePage https://github.com/microsoft/cascadia-code/releases/ *CascadiaCode-*
+$FolderName = $FileName -Split ".zip" | Select -First 1
+if (Test-Path -Path $FolderName) {
+    Remove-Item $FolderName -Recurse -Force
+}
+Expand-Archive $FileName
+Invoke-InstallFonts $FolderName CascadiaCodePL*.otf
+
+# Install Meslo Nerd Fonts patched for PowerLevel10k
+$MesloNerdFontsLinks = @("https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf",
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf",
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf",
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf");
+$MesloFontsFolder = "~/Downloads/MesloNerdFonts"
+if (Test-Path -Path $MesloFontsFolder) {
+    Remove-Item $MesloFontsFolder -Recurse -Force
+}
+New-Item $MesloFontsFolder -Type Directory -Force | Out-Null
+$replaceSearchString = "%20";
+$replaceWithString = " ";
+$splitBy = "/";
+#$MesloNerdFontsLinks | Where-Object { $s = "$MesloFontsFolder$($_.Split($splitBy)[-1].Replace($replaceString, $replaceWithString))"; Write-Host $s }
+
+$MesloNerdFontsLinks | `
+    ForEach-Object { 
+    $downloadPath = "$MesloFontsFolder/$($_.Split($splitBy)[-1].Replace($replaceString, $replaceWithString))";
+    Write-Host Downloading to $downloadPath
+    Invoke-WebRequest -URI $_ -OutFile $downloadPath
+}
+Invoke-InstallFonts MesloNerdFonts Meslo*.ttf
+
 
 # Install Az Powershell Model
 if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -ListAvailable)) {
@@ -14,61 +56,36 @@ else {
     Install-Module -Name Az -AllowClobber -Scope CurrentUser -Force
 }
 
+
+
+############## Installing and enabling WSL      ######################
+# Steps from https://docs.microsoft.com/en-us/windows/wsl/install-win10#manual-installation-steps
+# Step 1 - Enable the Windows Subsystem for Linux
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart#
+# Step 2 Install WSL and Ubuntu / Step 2 - Check requirements for running WSL 2
+choco install -y wsl wsl2 wsl-ubuntu-2004 
+# Step 3 - Enable Virtual Machine feature
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+# Step 4 - Download the Linux kernel update package
+
+# Install update to WSL
+DownloadAndInstall "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi"
+
+# Step 5 - Set WSL 2 as your default version
+wsl --set-default-version 2
+
 #Install CPU Z
 $cpuZContent = Invoke-WebRequest -Uri https://www.cpuid.com/softwares/cpu-z.html
 $taichiSetupPageLink = $cpuZContent.Links | ? { $_.href -like "*taichi*exe*" } | select href -First 1
 $latestTaichiSetupContent = Invoke-WebRequest -Uri $taichiSetupPageLink.href
 $latestTaichiSetupUri = ($latestTaichiSetupContent.Links | ? { $_.href -like "*taichi-en.exe*" } | Select href -First 1).href;
-cd ~/Downloads
 $cpuzTaichiInstallerName = DownloadFile $latestTaichiSetupUri
 Invoke-Expression "./$cpuzTaichiInstallerName /VERYSILENT /NORESTART"
 
 
 #Install CDIsplayEx
-cd ~/Downloads
 Invoke-WebRequest -Uri https://www.cdisplayex.com/findit.php -Method POST -OutFile CDisplay.exe
 .\CDisplay.exe /VERYSILENT
-
-# Install Cascadia Code PL Fonts
-$FileName = DownloadFromGithubReleasePage https://github.com/microsoft/cascadia-code/releases/ *CascadiaCode-*
-
-$FolderName = $FileName -Split ".zip" | Select -First 1
-if (Test-Path -Path $FolderName) {
-    Remove-Item $FolderName -Recurse -Force
-}
-Expand-Archive $FileName
-
-$Destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
-$SourceDir = ".\$FolderName\"
-
-$TempFolder = "~/Downloads/TempFonts"
-if (Test-Path -Path "TempFonts") {
-    Remove-Item "TempFonts" -Recurse -Force
-}
-New-Item $TempFolder -Type Directory -Force | Out-Null
-$FONTS = 0x14
-$objShell = New-Object -ComObject Shell.Application
-$objFolder = $objShell.Namespace($FONTS)
-Get-ChildItem -Path $SourceDir -Include 'CascadiaCodePL*.otf' -Recurse | ForEach {
-    $File = $_
-    $try = $true
-    $installedFonts = @(Get-ChildItem c:\windows\fonts | Where-Object { $_.PSIsContainer -eq $false } | Select-Object basename)
-    $name = $File.baseName
-
-    foreach ($font in $installedFonts) {
-        $font = $font -replace "_", ""
-        $name = $name -replace "_", ""
-        if ($font -match $name) {
-            $try = $false
-        }
-    }
-    
-    if ($try) {
-        $objFolder.CopyHere($File.fullname)
-        Write-Host ++++++++++ Installed $File.fullname
-    }
-    
-}
 
 # Test
 # Install Windows Power Toys
@@ -83,7 +100,6 @@ Invoke-Expression $SilentInstallCmd
 # Install Steam client
 $SteamSetupUrl = "https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe"
 $SteamSetupFileName = $SteamSetupUrl -Split "/" | Select -Last 1
-cd ~/Downloads
 if (Test-Path -Path $SteamSetupFileName) {
     Remove-Item $SteamSetupFileName 
 }
@@ -92,29 +108,8 @@ $SilentInstallCmd = "& .\$SteamSetupFileName /S"
 Invoke-Expression $SilentInstallCmd
 
 # Installe Battle.net
-CD ~/Downloads
 Invoke-WebRequest -Uri "https://us.battle.net/download/getInstaller?os=win&installer=Battle.net-Setup.exe" -OutFile Battle.net-Setup.exe
 ./Battle.net-Setup.exe
 # Install Surfshark vpn
-cd ~/Downloads
 DownloadFile "https://downloads.surfshark.com/windows/latest/SurfsharkSetup.exe";
 ./SurfsharkSetup.exe
-
-# Installing and enabling WSL
-# Steps from https://docs.microsoft.com/en-us/windows/wsl/install-win10#manual-installation-steps
-# Step 1 - Enable the Windows Subsystem for Linux
-dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart#
-# Step 2 Install WSL and Ubuntu / Step 2 - Check requirements for running WSL 2
-choco install -y wsl wsl2 wsl-ubuntu-2004 
-# Step 3 - Enable Virtual Machine feature
-dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-# Step 4 - Download the Linux kernel update package
-
-# Install update to WSL
-cd ~/Downloads
-DownloadAndInstall "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi"
-
-
-# Step 5 - Set WSL 2 as your default version
-wsl --set-default-version 2
-#Unity
