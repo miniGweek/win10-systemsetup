280dd6ef43fe99c3dde2946a8ffcd3fd1f985770
Rahul Sarkar <rsarkar@sxiq.com.au>
2021-03-21 14:33:26 +1100

    Refactoring + Adding an option to directly install msi
    - Adding option to traverse multiple pages and match links based on pattern and download files
    - Adding option to download installers and install them without depending on chocolatey
    - Using curl to download file as it follows redirect instead of Invoke-Webrequest

--- 00.CommonFunctions.ps1
+++ 00.CommonFunctions.ps1
@@ -1,8 +1,49 @@
 
+Function Invoke-DownloadByTraversingManyPages($startUrl, $patterns) {
+    $arrayOfPatterns = $patterns.Split(",");
+    $url = @($startUrl)
+    $uriObject = [System.Uri]$startUrl;
+    $uriHost = $uriObject.Host;
+    
+    $downloadedFileName = DownloadFileIfUrlEndsWithExeOrMSI $startUrl
+    if ($null -eq $downloadedFileName) {
+        foreach ($p in $arrayOfPatterns) {
+            Write-Host "+++++ Visiting $($url[-1])"
+            $page = Invoke-WebRequest -Uri $url[-1] #Always use the last url
+            Write-Host "+++++ Searching links with pattern $p"
+            $link = $page.Links | Where-Object { $_.href -like $p } | Select -First 1
+            Write-Host "+++++ Matched href $($link.href)"
+            $newUrl = Get-AbsoluteUrl $uriHost $link.href
+            $url += $newUrl
+            Write-Host "+++++ Link matched $newUrl"
+        }
+        $downloadedFileName = DownloadFileIfUrlEndsWithExeOrMSI $url[-1]
+    }
+    return $downloadedFileName
+}
+
+Function DownloadFileIfUrlEndsWithExeOrMSI($url) {
+    if ($url.EndsWith(".exe") -or $url.EndsWith(".msi")) {
+        Write-Host "+++++ Download link found - $url .Initiating download."
+        $fileName = DownloadFile $url $fileName
+        return $fileName
+    }
+    return $null
+}
+
+Function Get-AbsoluteUrl($uriHost, $uri) {
+    if ($uri.IndexOf("https://") -ne 0) {
+        return "https://$uriHost/$uri"
+    }
+    return $uri
+}
 function DownloadFile($url) {
     $installerLink = $url
     $installerName = $installerLink -Split "/"
-    Invoke-WebRequest $url -OutFile $installerName[-1]
+    if ($installerName[-1].IndexOf("=") -gt 0) {
+        $installerName = $installerName[-1].Split("=");
+    }
+    curl $url -Lo $installerName[-1]
     return $installerName[-1];
 }
 function InstallMSI($fileName) {
@@ -18,6 +59,17 @@ function InstallMSI($fileName) {
     )
     Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow 
 }
+
+
+Function SilentInstallExe($exeName) {
+    $shortDate = Get-Date -Format "ddMMyyyy"
+    Invoke-Expression ".\$exeName /VERYSILENT /NORESTART /LOG=$exeName.$shortDate.log"
+}
+
+Function SilentInstallExe2($exeName) {
+    Invoke-Expression ".\$exeName /S"
+}
+
 function DownloadAndInstall($url) {
     $installerName = DownloadFile $url
     InstallMSI $installerName
