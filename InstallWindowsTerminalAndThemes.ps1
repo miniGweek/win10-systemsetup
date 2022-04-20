bae85f0a05878902fa571f5ce471ad171455a66d
Rahul Sarkar <rsarkar@sxiq.com.au>
2022-04-20 17:37:41 +1000

    One powershell script to install themes

---
+++ InstallWindowsTerminalAndThemes.ps1
@@ -0,0 +1,11 @@
+Install-PackageProvider -Name "NuGet" -Force
+Install-Module -Name Microsoft.PowerShell.Archive -Force
+Import-Module Microsoft.PowerShell.Archive
+$CodeZipUrl = "https://github.com/miniGweek/win10-systemsetup/archive/refs/heads/main.zip"
+$DownloadFilePath = "$((Resolve-Path -Path "~/Downloads").Path)/win10-systemsetup.zip"
+$ExtractFolderPath = (Resolve-Path -Path "~/Downloads").Path
+
+Invoke-WebRequest -Uri $CodeZipUrl -UseBasicParsing -OutFile $DownloadFilePath
+Expand-Archive -LiteralPath $DownloadFilePath -DestinationPath $ExtractFolderPath
+Set-Location "$ExtractFolderPath\win10-systemsetup-main"
+&"$ExtractFolderPath\win10-systemsetup-main\07.SetupWindowsTerminaAndPowerShell.ps1"
