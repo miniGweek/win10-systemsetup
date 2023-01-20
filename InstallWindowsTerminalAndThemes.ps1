fa9570716b5981f349fb2ee1d9237a8595fb9e83
Rahul Sarkar <rahul.sarkar@kallipr.com>
2023-01-21 09:49:32 +1100

    refactor: branch name support added to script download and execute

--- InstallWindowsTerminalAndThemes.ps1
+++ InstallWindowsTerminalAndThemes.ps1
@@ -1,11 +1,17 @@
+param([string]$Branch)
 Install-PackageProvider -Name "NuGet" -Force
 Install-Module -Name Microsoft.PowerShell.Archive -Force
 Import-Module Microsoft.PowerShell.Archive
-$CodeZipUrl = "https://github.com/miniGweek/win10-systemsetup/archive/refs/heads/users/miniGweek/feature/azure_arc_script.zip"
+
+$CodeZipUrl = "https://github.com/miniGweek/win10-systemsetup/archive/refs/heads/$Branch.zip"
 $DownloadFilePath = "$((Resolve-Path -Path "~/Downloads").Path)/win10-systemsetup.zip"
 $ExtractFolderPath = (Resolve-Path -Path "~/Downloads").Path
 
 Invoke-WebRequest -Uri $CodeZipUrl -UseBasicParsing -OutFile $DownloadFilePath
 Expand-Archive -LiteralPath $DownloadFilePath -DestinationPath $ExtractFolderPath
-Set-Location "$ExtractFolderPath\win10-systemsetup-main"
+
+$SourceFolderPath = "win10-systemsetup-$($Branch -Replace '/','-')"
+Write-Host "Source folder path = $ExtractFolderPath\$SourceFolderPath"
+
+Set-Location "$ExtractFolderPath\$SourceFolderPath"
 &"$ExtractFolderPath\win10-systemsetup-main\07.SetupWindowsTerminaAndPowerShell.ps1"
