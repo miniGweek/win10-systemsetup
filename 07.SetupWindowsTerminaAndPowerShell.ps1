35100fada713bf72f9bf70e13562521845db82c7
Rahul Sarkar <rahul.sarkar@kallipr.com>
2023-01-21 09:16:17 +1100

    feat: add error handling
    - stop script executing if error was encountered
    - load common script from new location

--- 07.SetupWindowsTerminaAndPowerShell.ps1
+++ 07.SetupWindowsTerminaAndPowerShell.ps1
@@ -1,39 +1,53 @@
 $CurrentScriptPath = $MyInvocation.MyCommand.Path
 $CurrentScriptDir = Split-Path $CurrentScriptPath -Parent
 Write-Output "Current Script Directory is $CurrentScriptDir"
-. "$CurrentScriptDir\00.CommonFunctions.ps1"
 
-$CurrentDateTimeStamp = Get-Date -Format "ddMMyyyy_hhmmss"
-Start-Transcript -Path "~/Downloads/$($MyInvocation.MyCommand.Name).$CurrentDateTimeStamp.txt" -NoClobber
+$CurrentErrorActionPreference = $ErrorActionPreference
+$ErrorActionPreference = 'Stop'
+try {
+    $CommonFunctionScriptPath = "$CurrentScriptDir\common\CommonFunctions.ps1"
+    . "$CommonFunctionScriptPath"
 
-Set-Location ~/Downloads
+    Write-Log "Common functions loaded."
 
-# Install Chocolatey if it isn't installed 
-Write-Log -Message "Install Chocolatey package manager if it's not installed"
-$ChildScriptPath = "$CurrentScriptDir\08.InstallChocolatey.ps1"
-Start-Process "powershell.exe" -ArgumentList "-c $ChildScriptPath -ParentScriptDir $CurrentScriptDir" -Wait
+    $CurrentDateTimeStamp = Get-Date -Format "ddMMyyyy_hhmmss"
+    Start-Transcript -Path "~/Downloads/$($MyInvocation.MyCommand.Name).$CurrentDateTimeStamp.txt" -NoClobber
 
-Write-Log -Message "Install PowerShellCore if it's not installed"
-$ChildScriptPath = "$CurrentScriptDir\09.InstallPoweShellCore.ps1"
-Start-Process "powershell.exe" -ArgumentList "-c $ChildScriptPath -ParentScriptDir $CurrentScriptDir" -Wait
+    Set-Location ~/Downloads
 
-Write-Log -Message "Install WindowsTerminal"
-$ChildScriptPath = "$CurrentScriptDir\10.InstallWindowsTerminal.ps1"
-Start-Process "powershell.exe" -ArgumentList "-c $ChildScriptPath -ParentScriptDir $CurrentScriptDir" -Wait
+    # Install Chocolatey if it isn't installed 
+    Write-Log -Message "Install Chocolatey package manager if it's not installed"
+    $ChildScriptPath = "$CurrentScriptDir\08.InstallChocolatey.ps1"
+    Start-Process "powershell.exe" -ArgumentList "-c $ChildScriptPath -ParentScriptDir $CurrentScriptDir" -Wait
 
-$PowerShellInstallFolder = "C:\Program Files\PowerShell"
-$PowerShellExePath = Get-ExePath -RootSearchDirectory $PowerShellInstallFolder `
-    -ExeName "pwsh.exe" `
-    -Exclude "*preview*" `
-    -Recurse
+    Write-Log -Message "Install PowerShellCore if it's not installed"
+    $ChildScriptPath = "$CurrentScriptDir\09.InstallPoweShellCore.ps1"
+    Start-Process "powershell.exe" -ArgumentList "-c $ChildScriptPath -ParentScriptDir $CurrentScriptDir" -Wait
 
-$ChildScriptPath = "$CurrentScriptDir\11.InstallFonts.ps1"
-Start-Process $PowerShellExePath -ArgumentList "-c $ChildScriptPath -ParentScriptDir $CurrentScriptDir" -Wait
+    Write-Log -Message "Install WindowsTerminal"
+    $ChildScriptPath = "$CurrentScriptDir\10.InstallWindowsTerminal.ps1"
+    Start-Process "powershell.exe" -ArgumentList "-c $ChildScriptPath -ParentScriptDir $CurrentScriptDir" -Wait
 
-$ChildScriptPath = "$CurrentScriptDir\12.InstallOhMyPoshAndGit.ps1"
-Start-Process $PowerShellExePath -ArgumentList "-c $ChildScriptPath -ParentScriptDir $CurrentScriptDir" -Wait
+    $PowerShellInstallFolder = "C:\Program Files\PowerShell"
+    $PowerShellExePath = Get-ExePath -RootSearchDirectory $PowerShellInstallFolder `
+        -ExeName "pwsh.exe" `
+        -Exclude "*preview*" `
+        -Recurse
 
-$ChildScriptPath = "$CurrentScriptDir\13.UpdateWindowsTerminalSettings.ps1"
-Start-Process $PowerShellExePath -ArgumentList "-c $ChildScriptPath -ParentScriptDir $CurrentScriptDir" -Wait
+    $ChildScriptPath = "$CurrentScriptDir\11.InstallFonts.ps1"
+    Start-Process $PowerShellExePath -ArgumentList "-c $ChildScriptPath -ParentScriptDir $CurrentScriptDir" -Wait
 
-Stop-Transcript 
+    $ChildScriptPath = "$CurrentScriptDir\12.InstallOhMyPoshAndGit.ps1"
+    Start-Process $PowerShellExePath -ArgumentList "-c $ChildScriptPath -ParentScriptDir $CurrentScriptDir" -Wait
+
+    $ChildScriptPath = "$CurrentScriptDir\13.UpdateWindowsTerminalSettings.ps1"
+    Start-Process $PowerShellExePath -ArgumentList "-c $ChildScriptPath -ParentScriptDir $CurrentScriptDir" -Wait
+
+    Stop-Transcript 
+}
+catch {
+    Get-Error
+}
+finally {
+    $ErrorActionPreference = $CurrentErrorActionPreference  
+}
