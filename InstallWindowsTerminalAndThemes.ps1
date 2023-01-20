eca6178ca9124cf1ad56a60e0881d6096de624ad
Rahul Sarkar <rahul.sarkar@kallipr.com>
2023-01-21 09:18:38 +1100

    chore: slight change of path

--- InstallWindowsTerminalAndThemes.ps1
+++ InstallWindowsTerminalAndThemes.ps1
@@ -1,7 +1,7 @@
 Install-PackageProvider -Name "NuGet" -Force
 Install-Module -Name Microsoft.PowerShell.Archive -Force
 Import-Module Microsoft.PowerShell.Archive
-$CodeZipUrl = "https://github.com/miniGweek/win10-systemsetup/archive/refs/heads/main.zip"
+$CodeZipUrl = "https://github.com/miniGweek/win10-systemsetup/archive/refs/heads/users/miniGweek/feature/azure_arc_script.zip"
 $DownloadFilePath = "$((Resolve-Path -Path "~/Downloads").Path)/win10-systemsetup.zip"
 $ExtractFolderPath = (Resolve-Path -Path "~/Downloads").Path
 
