5eb7f5c2bf6035fa56b3a3f22687f135263668bc
Rahul Sarkar <rahul.sarkar@kallipr.com>
2023-01-21 09:50:47 +1100

    refactor: branch name support added to script download and execute

--- InstallWindowsTerminalAndThemes.ps1
+++ InstallWindowsTerminalAndThemes.ps1
@@ -14,4 +14,4 @@ $SourceFolderPath = "win10-systemsetup-$($Branch -Replace '/','-')"
 Write-Host "Source folder path = $ExtractFolderPath\$SourceFolderPath"
 
 Set-Location "$ExtractFolderPath\$SourceFolderPath"
-&"$ExtractFolderPath\win10-systemsetup-main\07.SetupWindowsTerminaAndPowerShell.ps1"
+& "$ExtractFolderPath\$SourceFolderPath\07.SetupWindowsTerminaAndPowerShell.ps1"
